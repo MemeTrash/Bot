307e3baf24cfb0969fcb2c0c7532c97f2a1ae95f
James Campbell <james@supmenow.com>
2016-09-15 15:17:36 +0100

    Much request.

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 var Bottr = require('bottr')
+var Request = require('request')
 
 var bot = new Bottr.Bot()
 
 bot.on('message_received', function(message, session) {
 
-  // - Make API Request
-  // - Send back an Image
+  Request.post('https://api.memetrash.co.uk/', {
+    form:{
+      text: message.text
+    }
+  }, function(err, httpResponse, body) {
 
-  session.send('Wow')
+    var response = JSON.parse(body);
+    session.send(null, {
+      type: 'image/jpeg',
+      url: response.url
+    })
+  })
 })
 
 bot.listen()
