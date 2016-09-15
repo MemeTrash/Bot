07e5c5f4f05bfd4f59884d22dc6cedb7fcad95e9
James Campbell <james@supmenow.com>
2016-09-15 15:03:27 +0100

    Wow.

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 var Bottr = require('bottr')
 
 var bot = new Bottr.Bot()
+
+bot.on('message_received', function(message, session) {
+
+  // - Make API Request
+  // - Send back an Image
+
+  session.send('Wow')
+})
+
 bot.listen()
